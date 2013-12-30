5e8bd10fa8237660f4efa3e52d5a4fd1b58841b6
Jake Schievink <jakeschievink@gmail.com>
2013-12-29 21:22:34 -0500

    Works now

--- main.py
+++ main.py
@@ -7,29 +7,57 @@ class SetupWindow(QDialog):
     def __init__(self):
         super(SetupWindow, self).__init__()
         container = QVBoxLayout()
-        hcontainer = QHBoxLayout()
-        self.inputTimeDelay = QLineEdit("Delay");
-        self.inputNumberOfPictures = QLineEdit("Number of Pictures");
+        delayContainer = QHBoxLayout()
+        numberOfPicturesContainer = QHBoxLayout()
+        self.inputTimeDelay = QSpinBox();
+        self.inputNumberOfPictures = QSpinBox()
         self.submitButton = QPushButton('Set Variables')
-        hcontainer.addWidget(QLabel("Input Delay:"))
-        hcontainer.addWidget(self.inputTimeDelay)
-        container.addLayout(hcontainer)
-        container.addWidget(self.inputNumberOfPictures)
+        delayContainer.addWidget(QLabel("Input Delay:"))
+        delayContainer.addWidget(self.inputTimeDelay)
+        numberOfPicturesContainer.addWidget(QLabel("Number Of Pictures to Take:"))
+        numberOfPicturesContainer.addWidget(self.inputNumberOfPictures)
+        container.addLayout(delayContainer)
+        container.addLayout(numberOfPicturesContainer)
         container.addWidget(self.submitButton)
+        self.setWindowTitle("Setup")
         self.setLayout(container)
+        self.submitButton.clicked.connect(self.submitValues)
         self.show()
+    def submitValues(self):
+        numOfPictures = self.inputNumberOfPictures.value()
+        delay = self.inputTimeDelay.value()
+        pictureInst = PictureDameon(delay, numOfPictures)
+        self.hide()
+        pictureInst.start()
 
+class PictureDameon:
+    def __init__(self, delay, numOfPictures):
+        self.delay = delay
+        self.numOfPictures = numOfPictures
+        self.camera = CameraPicture()
+    def start(self):
+        print "starting sleep"
+        time.sleep(float(self.delay))
+        self.takePicture()
+    def takePicture(self):
+        for i in range(0, self.numOfPictures):
+            print "taking picture"
+            self.camera.takePicture()
+            self.camera.savePicture()
+            time.sleep(1)
+        self.start()
 
-class CameraPicture():
+class CameraPicture:
     def  __init__(self):
         pygame.camera.init()
         self.cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
-    def takePicture():
-        cam.start()
-        img = self.cam.get_image()
-    def savePicture():
+    def takePicture(self):
+        self.cam.start()
+        self.img = self.cam.get_image()
+    def savePicture(self):
         ctime = time.localtime()
-        pygame.image.save(img, str(ctime.tm_mon)+":"+str(ctime.tm_day)+":"+str(ctime.tm_hour)+":"+str(ctime.tm_min)+":"+str(ctime.tm_sec)+"picture.jpg")
+        pygame.image.save(self.img, str(ctime.tm_mon)+":"+str(ctime.tm_mday)+":"+str(ctime.tm_hour)+":"+str(ctime.tm_min)+":"+str(ctime.tm_sec)+"picture.jpg")
+        self.cam.stop()
 
 
 if __name__ == '__main__':
